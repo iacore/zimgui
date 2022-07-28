e60c3cad61615cf6c90da9ed859fbdf32cff95ba
Christoffer Gustafsson <christoffer.golf@gmail.com>
2022-07-28 18:56:04 +0200

    Bugfix for build, the include path for imgui was not relative path. Also improved readme.

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # (incomplete) Zig Imgui bindings
 Bindings in zig for [Dear ImGui](https://github.com/ocornut/imgui). These are hand written and incomplete for now, while I work on a c++ parser in a side project.
 ## Usage
-See example usage in [zig-imgui-template](https://github.com/dumheter/zig-imgui-template/tree/zimgui).
+For complete example, see usage in [zig-imgui-template](https://github.com/dumheter/zig-imgui-template/tree/zimgui).
+### Backend
+Imgui doesnt do much on it own, it relies on a backend to handle rendering, and something to handle windows (and more). This repro has some code prepared for OpenGL3 as backend, and Glfw to handle windowing, via `addBackendGlfwOpenGl3`.
+### Example
+
+``` zig
+const zimgui = @import("deps/zimgui/build.zig");
+
+pub fn build(b: *Builder) void {
+    var exe = ...;
+
+    exe.addPackagePath("zimgui", "deps/zimgui/src/zimgui.zig");
+    _ = zimgui.link(b, exe);
+
+    exe.addPackagePath("zimgui_backend", "deps/zimgui/src/backend_glfw_opengl3.zig");
+    addBackendGlfwOpenGl3(b, exe);
+
+    ...
+}
+```
--- build.zig
+++ build.zig
@@ -1,21 +1,31 @@
 const Builder = @import("std").build.Builder;
 const std = @import("std");
 
-pub fn build(b: *Builder) void {
-    const mode = b.standardReleaseOptions();
-
-    const main_tests = b.addTest("src/zimgui.zig");
-    main_tests.setBuildMode(mode);
-    main_tests.linkLibrary(link(b, main_tests));
-
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
-}
-
+/// Build and link zig imgui bindings.
+///
+/// Remeber to also call 
+/// `exe.addPackagePath("zimgui", "deps/zimgui/src/zimgui.zig");`
+///
 pub fn link(b: *Builder, step: *std.build.LibExeObjStep) *std.build.LibExeObjStep {
     const lib = buildLibrary(b, step);
     step.linkLibrary(lib);
     return lib;
+}
+
+/// Use OpenGL3 as rendering backend, and Glfw for windowing.
+///
+/// Remeber to also call 
+/// `exe.addPackagePath("zimgui_backend", "deps/zimgui/src/backend_glfw_opengl3.zig");`
+/// 
+pub fn addBackendGlfwOpenGl3(b: *Builder, exe: *std.build.LibExeObjStep) void {
+    exe.addCSourceFiles(&[_][]const u8{
+        relativePath(b, "src/backend_glfw_opengl3.cpp"),
+        relativePath(b, "deps/imgui/backends/imgui_impl_glfw.cpp"),
+        relativePath(b, "deps/imgui/backends/imgui_impl_opengl3.cpp"),
+        }, &[_][]const u8{});
+
+    exe.addIncludeDir(relativePath(b, "deps/imgui"));
+    exe.linkLibCpp();
 }
 
 fn relativePath(b: *Builder, local_path: []const u8) []const u8 {
@@ -40,23 +50,9 @@ fn buildLibrary(b: *Builder, step: *std.build.LibExeObjStep) *std.build.LibExeOb
     step.defineCMacro("IMGUI_DISABLE_OBSOLETE_KEYIO", null);
     step.defineCMacro("IMGUI_DISABLE_OBSOLETE_FUNCTIONS", null);
 
+    lib.addIncludeDir(relativePath(b, "deps/imgui"));
     lib.linkLibCpp();
-    lib.addIncludeDir("deps/imgui");
 
     lib.install();
     return lib;
-}
-
-/// Remeber to also call
-/// `exe.addPackagePath("zimgui_backend", "deps/zimgui/src/backend_glfw_opengl3.zig");`
-/// 
-pub fn addBackendGlfwOpenGl3(b: *Builder, exe: *std.build.LibExeObjStep) void {
-    exe.addCSourceFiles(&[_][]const u8{
-        relativePath(b, "src/backend_glfw_opengl3.cpp"),
-        relativePath(b, "deps/imgui/backends/imgui_impl_glfw.cpp"),
-        relativePath(b, "deps/imgui/backends/imgui_impl_opengl3.cpp"),
-        }, &[_][]const u8{});
-
-    exe.linkLibCpp();
-    exe.addIncludeDir(relativePath(b, "deps/imgui"));
 }
